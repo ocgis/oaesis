ba3558cd6e0fef7d01a4eab87707fc1b21428b8b
cg <cg>
1999-11-05 17:44:43 +0000

    - Added oaesis.h, which contains a prototype for the callback handler   installer. - Added oaesis.h to install in the osis include directory

---
+++ src/lib/oaesis.h
@@ -0,0 +1,25 @@
+/*
+** aesbind.h
+**
+** Copyright 1998-1999 Christer Gustavsson <cg@nocrew.org>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**  
+** Read the file COPYING for more information.
+**
+*/
+
+#ifndef _OAESIS_H_
+#define _OAESIS_H_
+
+/*
+** Description
+** Install a callback handler
+*/
+void
+Oaesis_callback_handler(void * callback_handler);
+
+#endif /* _OAESIS_H_ */
