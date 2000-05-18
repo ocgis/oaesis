37b921df12ed699dd9251c790a9693419ac77827
cg <cg>
2000-05-18 12:27:01 +0000

    - Fixed referencing bug

--- src/lib/evnt.c
+++ src/lib/evnt.c
@@ -163,7 +163,7 @@ Evnt_do_mesag(WORD     apid,
     
   ei.events = MU_MESAG;
 
-  Evnt_do_multi(apid, &ei, &buf, &eo, 0, DONT_HANDLE_MENU_BAR);
+  Evnt_do_multi(apid, &ei, buf, &eo, 0, DONT_HANDLE_MENU_BAR);
   
   return 1;
 }
